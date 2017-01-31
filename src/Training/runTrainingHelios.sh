a396bad9acae2f5256af680a6b6e57970b79bab3
lupoglaz <lupoglaz@gmail.com>
2017-01-31 18:34:35 -0500

    Unfolding learning rate

--- src/Training/runTrainingHelios.sh
+++ src/Training/runTrainingHelios.sh
@@ -2,7 +2,7 @@
 #PBS -N TestDeepFolder
 #PBS -A ukg-030-aa
 #PBS -l walltime=300
-#PBS -l nodes=1:gpus=1
+#PBS -l nodes=1:gpus=4
 #PBS -l feature=k80
 #PBS -r n
 cd "${SCRATCH}/lupoglaz/deep_folder/src/Training"
@@ -12,14 +12,60 @@ nvidia-smi
 th TorchTrainRankingHomogeniousDataset.lua \
 -model_name ranking_model_8 \
 -dataset_name CASP_SCWRL \
--experiment_name QA_5 \
+-experiment_name LearningRate_1em2 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.01 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 100 \
+-model_save_period 1 \
+-max_epoch 150 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 1
+
+th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_1em3 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.001 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 100 \
+-model_save_period 1 \
+-max_epoch 150 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 2
+
+th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_1em4 \
 -datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
 -learning_rate 0.0001 \
 -l1_coef 0.00001 \
 -tm_score_threshold 0.01 \
 -gap_weight 0.1 \
--validation_period 10 \
--model_save_period 10 \
+-validation_period 100 \
+-model_save_period 1 \
 -max_epoch 150 \
 -decoys_ranking_mode gdt-ts \
--gpu_num 1
+-gpu_num 3
+
+th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_1em5 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.00001 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 100 \
+-model_save_period 1 \
+-max_epoch 150 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 4
+
