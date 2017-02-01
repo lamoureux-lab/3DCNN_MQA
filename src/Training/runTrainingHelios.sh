add1bdc30a09a76db6d0eaf92a903db30c6872de
lupoglaz <lupoglaz@gmail.com>
2017-01-31 19:15:02 -0500

    Run launch

--- src/Training/runTrainingHelios.sh
+++ src/Training/runTrainingHelios.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N TestDeepFolder
 #PBS -A ukg-030-aa
-#PBS -l walltime=300
-#PBS -l nodes=1:gpus=4
+#PBS -l walltime=43000
+#PBS -l nodes=1:gpus=8
 #PBS -l feature=k80
 #PBS -r n
 cd "${SCRATCH}/lupoglaz/deep_folder/src/Training"
@@ -21,11 +21,26 @@ CUDA_VISIBLE_DEVICES=0 th TorchTrainRankingHomogeniousDataset.lua \
 -gap_weight 0.1 \
 -validation_period 10 \
 -model_save_period 10 \
--max_epoch 70 \
+-max_epoch 100 \
 -decoys_ranking_mode gdt-ts \
 -gpu_num 1 > output1 &
 
 CUDA_VISIBLE_DEVICES=1 th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_05em2 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.005 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 10 \
+-model_save_period 10 \
+-max_epoch 100 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 1 > output2 &
+
+CUDA_VISIBLE_DEVICES=2 th TorchTrainRankingHomogeniousDataset.lua \
 -model_name ranking_model_8 \
 -dataset_name CASP_SCWRL \
 -experiment_name LearningRate_1em3 \
@@ -36,11 +51,26 @@ CUDA_VISIBLE_DEVICES=1 th TorchTrainRankingHomogeniousDataset.lua \
 -gap_weight 0.1 \
 -validation_period 10 \
 -model_save_period 10 \
--max_epoch 70 \
+-max_epoch 100 \
 -decoys_ranking_mode gdt-ts \
--gpu_num 1 > output2 &
+-gpu_num 1 > output3 &
 
-CUDA_VISIBLE_DEVICES=2 th TorchTrainRankingHomogeniousDataset.lua \
+CUDA_VISIBLE_DEVICES=3 th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_05em3 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.0005 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 10 \
+-model_save_period 10 \
+-max_epoch 100 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 1 > output4 &
+
+CUDA_VISIBLE_DEVICES=4 th TorchTrainRankingHomogeniousDataset.lua \
 -model_name ranking_model_8 \
 -dataset_name CASP_SCWRL \
 -experiment_name LearningRate_1em4 \
@@ -51,11 +81,26 @@ CUDA_VISIBLE_DEVICES=2 th TorchTrainRankingHomogeniousDataset.lua \
 -gap_weight 0.1 \
 -validation_period 10 \
 -model_save_period 10 \
--max_epoch 70 \
+-max_epoch 100 \
 -decoys_ranking_mode gdt-ts \
--gpu_num 1 > output3 &
+-gpu_num 1 > output5 &
 
-CUDA_VISIBLE_DEVICES=3 th TorchTrainRankingHomogeniousDataset.lua \
+CUDA_VISIBLE_DEVICES=5 th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_05em4 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.00005 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 10 \
+-model_save_period 10 \
+-max_epoch 100 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 1 > output6 &
+
+CUDA_VISIBLE_DEVICES=6 th TorchTrainRankingHomogeniousDataset.lua \
 -model_name ranking_model_8 \
 -dataset_name CASP_SCWRL \
 -experiment_name LearningRate_1em5 \
@@ -66,8 +111,23 @@ CUDA_VISIBLE_DEVICES=3 th TorchTrainRankingHomogeniousDataset.lua \
 -gap_weight 0.1 \
 -validation_period 10 \
 -model_save_period 10 \
--max_epoch 70 \
+-max_epoch 100 \
 -decoys_ranking_mode gdt-ts \
--gpu_num 1 > output4 &
+-gpu_num 1 > output7 &
+
+CUDA_VISIBLE_DEVICES=7 th TorchTrainRankingHomogeniousDataset.lua \
+-model_name ranking_model_8 \
+-dataset_name CASP_SCWRL \
+-experiment_name LearningRate_03em2 \
+-datasets_dir /scratch/ukg-030-aa/lupoglaz/ \
+-learning_rate 0.005 \
+-l1_coef 0.00001 \
+-tm_score_threshold 0.01 \
+-gap_weight 0.1 \
+-validation_period 10 \
+-model_save_period 10 \
+-max_epoch 100 \
+-decoys_ranking_mode gdt-ts \
+-gpu_num 1 > output8 &
 
 wait
